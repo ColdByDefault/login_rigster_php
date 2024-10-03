5249f3bae12d77a2ba3af8cd0d0523f733f61e44
koni <152221158+konikonikoni@users.noreply.github.com>
2024-10-03 10:15:00 +0200

    Bla

--- com/view/burgerMenue.php
+++ com/view/burgerMenue.php
@@ -3,12 +3,12 @@
 <!-- Das Dropdown-Menü -->
 <div class="dropdown" id="menuDropdown">
     <div class="dropdown-form">
-        <form>
+        <form id="burgerMenuInput">
             <input type="text">
             <input type="password">
             <input type="submit">
-            <a href="#">Registrieren</a>
         </form>
+        <a href="#">Registrieren</a>
     </div>
     <div class="dropdown-item" onclick="window.location.href='';">Persönliche Daten</div>
     <div class="dropdown-item" onclick="window.location.href='';">Termin buchen</div>
@@ -19,14 +19,19 @@
     // JavaScript, um das Dropdown-Menü anzuzeigen/verbergen
     const menuIcon = document.getElementById("menuIcon");
     const menuDropdown = document.getElementById("menuDropdown");
+    const burgerMenuInput = document.getElement);
 
     menuIcon.addEventListener("click", function() {
+        if (menuDropdown.style.display === "block") {
+            menuDropdown.style.display = "none";
+        } else {
             menuDropdown.style.display = "block";
+        }
     });
 
     // Schließen des Dropdown-Menüs, wenn außerhalb geklickt wird
     window.addEventListener("click", function(event) {
-        if (event.target !== menuIcon && event.target !== menuDropdown) {
+        if (event.target !== menuIcon && event.target !== menuDropdown && event.target !== burgerMenuInput) {
             menuDropdown.style.display = "none";
         }
     });
